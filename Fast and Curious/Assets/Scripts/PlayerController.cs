01d766b486384885f8f387945f60d7c727127cb2
HanHangit <clonkcool@yahoo.de>
2016-02-06 14:52:06 +0100

    PlayerController

---
+++ Fast and Curious/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float acceleration;
+    public float steering;
+    public float brake;
+    public float maxSpeed;
+
+    private Rigidbody rigid;
+    private float direction;
+    private float speed;
+    private Vector2 MovingDirection;
+	// Use this for nitialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+        direction = transform.rotation.y;
+        MovingDirection = new Vector2(1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Vertical") && speed < maxSpeed)
+        {
+            rigid.AddForce(MovingDirection * acceleration);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        speed = new Vector2(rigid.velocity.x, rigid.velocity.z).magnitude;
+        direction = transform.rotation.y;
+    }
+}
